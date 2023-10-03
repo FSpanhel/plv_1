70ad4b9bc30b5412506cc2dea239a1e75187e5cd
Fabian Spanhel <fabian.spanhel@prosiebensat1.com>
2023-10-03 13:18:46 +0200

    Add stationarity proof

--- _notebook_as_py/presentation.py
+++ _notebook_as_py/presentation.py
@@ -15,7 +15,12 @@
 
 # %% slideshow={"slide_type": "skip"}
 from IPython.display import display, HTML
-display(HTML("<style>.container { width:95% !important; }</style>"))
+# display(HTML("<style>.container { width:95% !important; }</style>"))
+
+from plv.data import load_verbraucherpreisindex
+from plv.plot import plot_ts
+
+inflation = load_verbraucherpreisindex(filter_columns=["inflation"])
 
 # %% [markdown] slideshow={"slide_type": "slide"}
 # [TODO] 
@@ -42,13 +47,20 @@ display(HTML("<style>.container { width:95% !important; }</style>"))
 # [TODO] Evtl. TOC?
 # - About me
 # - Einführung und Motivation [5 Min]
+# - Illustration des Zusammenhangs zwischen Zeitreihendaten und -prozess
 # - Stationarität
 #     - Definition [4 Min]
 #     - Illustration anhand AR(1) [6 Min]
-#     - Testen von Stationarität [4 Min]
+#     - Testen von Stationarität [4 Min] -> Low Priority
 # - Praktische Anwendung auf X Daten [10 Min]
 
-# %% [markdown] slideshow={"slide_type": "slide"}
+# %% [markdown] slideshow={"slide_type": "notes"}
+# - Lernziele sind, dass die Studierende den Zusammenhang zwischen Prozess und Daten verstehen 
+# - Die Definition der schwache Stationariät kennen
+# - Den AR(1) Prozess kennenlernen und wann er stationär ist
+# - Eine praktische Anwendung einens stationären AR(1) Prozess sehen
+
+# %% [markdown] slideshow={"slide_type": "notes"}
 # # About me [TODO: Auf Deutsch]
 # <br>
 # <br>
@@ -66,34 +78,105 @@ display(HTML("<style>.container { width:95% !important; }</style>"))
 
 # %% [markdown] slideshow={"slide_type": "slide"}
 # # Einführung und Motivation
+# <br>
+# <br>
+# <br>
+# <br>
+# <br>
+# <br>
+# <br>
+# <br>
+# <br>
+# <br>
+
+# %% [markdown] slideshow={"slide_type": "notes"}
+# - Wer von Ihnen hat einen Indexmietvertrag? 
+#     Mit einer Indexmiete vereinbaren Mieter und Vermieter bereits im Mietvertrag, dass sich die Kaltmiete erhöht, wenn die Verbraucherpreise steigen. Die Preise ermittelt regelmäßig das Statistische Bundesamt und veröffentlicht sie in dem sogenannten Verbraucherpreisindex (VPI).
+# - Dann teilen vermutlich ein paar unter Ihnen mein Leid, meine Miete wird sich im nächsten Monat nämlich um 12.2 % erhöhnen.
+# - Wie konnte es soweit kommen dass die Inflationsrate seit 2 Jahren auf den höchsten Stand seit der Wiedervereinigung gestiegen ist? [Corona und die Wirtschaftskrise] haben dazu geführt, dass die Verbraucherpreise, also die Inflation steigen. Weltweit steigen die Produktionskosten, die Lieferketten sind seit der Corona-Pandemie gestört. Infolge des Krieges in der Ukraine kommt es zu Rohstoffengpässen, die das Angebot verknappen – vor allem bei Energie und Getreide.
+#     - Die Erzeugerpreise sind extrem stark angestiegen und nach und nach reichen die Produzenten diese Preisanstiege an die Verbraucher weiter. Bisher sind es vor allem die Energiepreise, die sich in den Verbraucherpreisen direkt niedergeschlagen haben. Aber da sollte man sich nicht täuschen lassen.
+#     - https://zdfheute-stories-scroll.zdf.de/inflation-preise-teuer/index.html
+# - Grafik Inflation
+#     - Was bedeutet monatliche Inflation: Die Veränderung des Verbraucherpreisindex zum Vorjahr wird als Inflationsrate bezeichnet
+
+# %% slideshow={"slide_type": "skip"}
+import matplotlib as mpl
+mpl.rcParams['font.size'] = 18
+
+# %% slideshow={"slide_type": "slide"} hide_input=false
+plot_ts(
+    data=inflation,
+    title="Inflationsrate: Prozentuale Veränderung des Verbraucherpreisindex zum Vormonat. Quelle: https://www.destatis.de/",
+    size=(20, 8)
+)
+# https://www-genesis.destatis.de/genesis/online?sequenz=tabelleErgebnis&selectionname=61111-0002&startjahr=1996#abreadcrumb
+
+# %% slideshow={"slide_type": "slide"} cell_style="split" hide_input=true
+plot_ts(
+    data=inflation,
+    title="Inflationsrate: Prozentuale Veränderung des Verbraucherpreisindex zum Vormonat",
+    size=(20, 18)
+)
+
+# %% [markdown] slideshow={"slide_type": "-"} cell_style="split"
+# - In der Praxis ist es häufig der Fall, dass Daten zu aufeinanderfolgenden Zeitpunkten mit gleichmäßigen Abstand erhoben werden.
+# - Diese Daten bezeichnen man als **Zeitreihe**.
+# - Anders als in Querschnittstudien, gibt es durch die Zeit eine natürlich Reihenfolge der Beobachtungen.
+# - Dies führt in der Regel dazu, dass Beobachtungen, die zeitlich nahe beieinander liegen, enger miteinander verbunden sind als Beobachtungen, die weiter auseinander liegen.
+# - **Die Zeitreihenanalyse** befasst sich primär mit der Vorhersage von Zeitreihen.
+
+# %% slideshow={"slide_type": "notes"}
+- Wirtschaftsdaten (Börsenkurse, Zinsindizes, Absatzzahlen), Wetterdaten, Unternehmensdaten
+Börsenkurse allgemein, Bevölkerungsentwicklung, Preisindex, Wahlabsichtsbefragungen, Wetterdaten, Zinsindex
+- "Die Zeitreihenanalyse befasst sich in der Statistik mit der inferenzstatistischen Analyse von Zeitreihen und der Vorhersage von Trends (Trendextrapolation) zu ihrer künftigen Entwicklung"
+- Abhängigkeit durch Zeit an Inflation erklären: Das was in 2008 war relativ egal, was in letzter Zeit passiert
+
+# %% slideshow={"slide_type": "notes"}
+- Hamilton lesen
+- [x] Übungsunterlagen lesen
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# Im Folgenden, sei $T \subset \mathbb{T} \subset \mathbb{R}$.
+#
 # Wenn wir von einer (diskreten) Zeitreihe sprechen, können damit zwei verschiedene Begriffe gemeint sein:
-# 1. Eine Zeitreihe ist eine Abfolge von Daten, die in zeitlicher Reihenfolge angeordnet sind -> Beispiele.
-# 2. Eine Zeitreihe ist ein stochastischer Prozess, d.h., ein Folge von Zufallsvariablen mit einem Index der für Zeitpunkte steht.
+# 1. Eine Zeitreihe ist eine Abfolge von Daten $(y_t)_{t=1,...,T}$, die in zeitlicher Reihenfolge angeordnet sind.
+# 2. Eine Zeitreihe ist ein stochastischer Prozess $(Y_t)_{t\in \mathbb{T}}$, d.h., ein Folge von Zufallsvariablen mit einem Index $t$, der für Zeitpunkte steht.
 #
-# **Die Verbindung zwischen 1. und 2. ergibt sich dadruch, dass (in der Statistik) Daten in 1. als eine Stichprobe oder Realisierung eines zugrunde liegenden stochastischen Prozess in 2. aufgefasst werden.**
-#
-#
-# Eine (diskrete) Zeitreihe ist eine zeitlich geordnete Folge statistischer Maßzahlen.
+# **Die Verbindung zwischen 1. und 2. ergibt sich dadurch, dass Daten $(y_t)_{t=1,...,T}$ in 1. als eine Stichprobe oder Realisierung eines zugrunde liegenden stochastischen Prozesses $(Y_t)_{t\in \mathbb{T}}$ in 2. aufgefasst werden.**
 #
 #
-# äquidistant
-#
-# Warum Zeitreihen modellieren sinnvoll ist
 
 # %% [markdown] slideshow={"slide_type": "slide"}
 # ## Illustration des Zusammenhangs zwischen Zeitreihendaten und -prozess
 
-# %% [markdown]
-# ## Annahmen an einen Zeitreihenprozess für die statistische Inferenz und Modellierung
+# %% slideshow={"slide_type": "skip"}
+- Wichtig dass zu verstehen, in meiner Erfahrung war das unklar für manche Studierende oder sogar Doktoranden, dann ist auch alles leichter
 
-# %%
-Für die statistische Inferenz mit Zeitreihen müssen Annahmen getroffen werden, da in der Praxis meist nur eine Realisierung des die Zeitreihe generierenden Prozesses vorliegt. Die Annahme der Ergodizität bedeutet, dass Stichprobenmomente, die aus einer endlichen Zeitreihe gewonnen werden, für 
-�
-→
-∞T\rightarrow \infty  quasi gegen die Momente der Grundgesamtheit konvergieren.
+# %% [markdown] slideshow={"slide_type": "slide"}
+# ## Annahmen an einen Zeitreihenprozess für die statistische Inferenz und Modellierung
+# [TODO] Diese Folie weg oder woanders
+# Für die statistische Inferenz mit Zeitreihen müssen Annahmen getroffen werden, da in der Praxis meist nur eine Realisierung des die Zeitreihe generierenden Prozesses vorliegt. Die Annahme der Ergodizität bedeutet, dass Stichprobenmomente, die aus einer endlichen Zeitreihe gewonnen werden, für 
+# �
+# →
+# ∞T\rightarrow \infty  quasi gegen die Momente der Grundgesamtheit konvergieren.
 
 # %% [markdown] slideshow={"slide_type": "slide"}
 # # Stationarität
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# - In der Literatur gibt es 2 Definition von Stationarität, die Klasse der stochastischen Prozess einschränken
+# - Wir konzentieren uns hier auf die sogenannte schwache Stationarität
+#
+# > **DEFINITION:** (Schwache Stationarität) <br>
+# > Ein stochastischer Prozess $(Y_t)_{t\in \mathbb{T}}$ ist schwach stationär $:\!\!\iff$
+# >
+# > 1. $E[Y_t] = \mu \in \mathbb{R}$ <br>
+# > 2. $Cov[Y_t, Y_{t-h}] = \gamma(h) \in \mathbb{R}$
+# >
+# > für alle $\forall t \in \mathbb{T}$ und $\forall h \in \mathbb{T}$.
+
+# %% [markdown] slideshow={"slide_type": "notes"}
+# - Bei beiden Typen von Prozessen besitzen die endlichdimensionalen Verteilungen des Prozesses bestimmte zeitunabhängige Eigenschaften. Diese beziehen sich bei der Stationarität im engeren Sinn auf die gesamte Verteilungsgestalt und bei der Stationarität im weiteren Sinn nur auf die ersten beiden Momente der endlichdimensionalen Verteilungen
 
 # %% [markdown] slideshow={"slide_type": "slide"}
 # ## (Strikte Stationarität) und Kovarianzstationarität 
@@ -104,8 +187,11 @@ Für die statistische Inferenz mit Zeitreihen müssen Annahmen getroffen werden,
 # %% [markdown] slideshow={"slide_type": "notes"}
 # Evtl. Beispiel für Zeitreihenprozesse, sind die stationär? Darüber abstimmen, mit ja/nein/weiß nicht/kommt drauf an (Es reicht wenn sie die Erwartungs- und Varianzstationarität überprüfen)
 # 1. iid Exponential
+# 1. Summe zweier schwach stationärer Prozesse die voneinander unabhängig sind
 # 2. MA(1)
 # 3. AR(1)
+#
+# Laut Andi auch Plots zeigen von nicht stationären Prozessen
 
 # %% [markdown] slideshow={"slide_type": "slide"}
 # ## Illustration anhand der Simulation eines (linearen) AR(1) Prozesses
@@ -117,6 +203,7 @@ Für die statistische Inferenz mit Zeitreihen müssen Annahmen getroffen werden,
 #     1. Was ist wenn a negativ ist? Interessant weil ich sehr gut schätzen kann (Bild mit Normalverteilung)
 # 1. Simulationsbilder in Abhängigkeit von a
 #     1. Simulation nicht stationär weil wir immer mit gleichen Wert starten, aber schauen wir mal was passiert
+#     1. Evtl. auch erst als Loop und dann durch Matrixmultiplikation effizienter.
 # 1.	Theoretischen Beweis skizzieren (schon sophisticated, bei Copulas einfacher)
 #     1. Nur mean stationarität zeigen
 #     1.	Beweis für Kovarianzstationarität analog (Kovarianz auch nur Erwartungswert), oder E[Y_tY_{t-h}^k], k = 0, 1
@@ -125,7 +212,85 @@ Für die statistische Inferenz mit Zeitreihen müssen Annahmen getroffen werden,
 #         2.	Keine geschlossene Form für die Multivariate Verteilung im allgemeinen (nur durch Simulationen möglich)
 
 # %% [markdown] slideshow={"slide_type": "slide"}
-# ## Testen auf Stationarität
+# ## Skizze des Beweis für die schwache Stationarität des AR(1) Prozesses
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# ---
+# **THEOREM:** (Hinreichende Bedingungen für die Stationarität eines AR(1) Prozesses) <br>
+# Sei $Y_t = aY_{t-1} + U_t$, wobei $(U_t)_{t \in \mathbb{Z}}$ ein schwach stationärer stochastischer Prozess ist, so dass $Cov[U_t, U_h]=\sigma_U^2$ für $t=h$ und sonst $Cov[U_t, U_h]=0$ alle $t, h \in \mathbb{Z}$,  und $\sup_{t \in \mathbb{Z}} E[Y_t^2] < \infty$. <br><br>
+# Falls $|a| < 1$,  dann ist $(Y_t)_{t \in \mathbb{Z}}$ schwach stationär.
+# ---
+# - Der Bweis dieses Theorems ist nicht trivial.
+# - Im Folgenden werden die zwei Schritte deshalb nur skizziert.
+#     1. Limes Repräsentation von $Y_t$ als eine Funktion von $(U_t)_{t \in \mathbb{Z}}$.
+#     2. Berechung des Erwartungswerts und Kovarianzfunktion von $(Y_t)_{t \in \mathbb{Z}}$.
+
+# %% slideshow={"slide_type": "notes"}
+In der Literatur so nicht so einfach zu finden und nicht exakt
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# **Schritt 1: Limes Repräsentation von $Y_t$ als eine Funktion von $(U_t)_{t \in \mathbb{Z}}$.**
+#
+# Rekursive Substituierung führt zu
+# $$
+# Y_t = aY_{t-1} + U_t = a(aY_{t-2} + U_{t-1}) + U_t = \ldots = \underbrace{a^h Y_{t-h}}_{=:A_h} + \underbrace{\sum_{i=0}^{h-1}a^{i}U_{t-i}}_{=:B_h} =: X_h
+# $$
+# Was passiert mit $X_h$, wenn $h\to\infty$?
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# &#9888;&#65039; Da $X_h:=A_h + B_h$ eine Zufallsvariable ist, müssen wir zunächst eine Konvergenzart wählen.
+# <br>
+# ---
+# **DEFINITION:** (Konvergenz im quadratischen Mittel) <br>
+# Eine Folge von Zufallsvariable $(X_t)_{t\in\mathbb{Z}}$ konvergiert im quadratischen Mittel gegen eine Zufallsvariable $X$ $:\!\!\iff$
+# $\lim_{t\to\infty} E[(X_t-X)^2] = 0$.
+# <br>
+# In diesem Fall schreiben wir $X_t \stackrel{2}{\to} X$.
+# ---
+# - Man kann zeigen, dass $A_t + B_t \stackrel{2}{\to} A + B$ falls $A_t \stackrel{2}{\to} A$ und  $B_t \stackrel{2}{\to} B$. (Umdrehen)
+# - Wir können also die Konvergenz von $A_h$ und $B_h$ separat betrachten.
+# - Dass $A_h \stackrel{2}{\to} 0$, wenn $|a|<1$ und $\sup_{t \in \mathbb{Z}} E[Y_t^2] < \infty$, lässt sich relativ leicht zeigen.
+# - Dass eine Zufallsvariable $B \in L^2$ existiert, gegen die $B_h$ im quadratischen Mittel konvergiert, folgt daraus, dass der $L^2$ Raum vollständig ist und man zeigen kann, dass $B_h$ eine Cauchy Folge ist falls $|a| < 1$ und $(U_t)_{t\in\mathbb{Z}}$ weißes Rauschen ist.
+# - Insgesamt folgt also $\exists X \in L^2\colon X_h \stackrel{2}{\to} X$.
+#
+
+# %% [markdown] slideshow={"slide_type": "notes"}
+# - One way to define the distance between X_t and X is the expectation of the squared difference.
+# - Das ist für uns hilfreich da wir ja später an dem Erwartungswert und der Varianz des Limes interessiert sind.
+# - [Dreiecksungleichung](https://math.stackexchange.com/questions/4281919/convergence-of-a-linear-combination-of-random-variables), diese kann ich nutzen weil E[(X_t-X)^2] = L2_Norm^2 und die Dreiecksungleichung für die L2_Norm gilt.
+# - Frage: Wer weiß noch alles was eine Cauchy Folge ist?
+# - Wieso ist $B_h$ eine Cauchy Folge
+# $B_h \text{ is Cauchy } \rightarrow E[(B_m-B_n)^2] < \epsilon$
+# $E[(\sum_{i=N+1}^Ma_iU_{t-i})^2] \stackrel{U is WN}{=} \sum_{i=N+1}^M a^{2i}E[U_{t-i}^2] = E[U_{t-i}^2]\underbrace{\sum_{i=N+1}^M a^{2i}}_{Cauchy}$
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# **Schritt 2:Berechung des Erwartungswerts und Kovarianzfunktion von $(Y_t)_{t \in \mathbb{Z}}$.**
+#
+# Durch die Anwendung des [Fubini-Tonelli Theorems](https://en.wikipedia.org/wiki/Fubini's_theorem#Fubini-Tonelli) und der Tatsache, dass $\sum_{i=0}^{\infty}|a^{i}|\in \mathbb{R}$, falls $|a|<1$, und $(U_t)_{t\in\mathbb{Z}}$ Weißes Rauschen ist, folgt
+#
+# $$
+# \begin{align*}
+# E[Y_t] & = E[\sum_{i=0}^{\infty}a^{i}U_{t-i}] \stackrel{\text{Fubini-Tonelli},\ |a|<1}{=} \sum_{i=0}^{\infty}a^{i}\underbrace{E[U_{t-i}]}_{=\ 0} = 0
+# \end{align*}
+# $$
+#
+#
+# $$
+# \gamma(t, h) = Cov[Y_t, Y_{t-h}] = E[\sum_{i=0}^{\infty}a^{i}U_{t-i} \sum_{j=0}^{\infty}a^{j}U_{t-h-j}] \stackrel{\text{Fubini-Tonelli},\ |a|<1}{=}\sum_{i=0}^{\infty}\sum_{j=0}^{\infty}a^{i}a^{j}\underbrace{E[U_{t-i}U_{t-h-j}}_{\sigma_U^2 \text{ if } i = h + j, \text{ else } 0}]
+# \\
+# \stackrel{i:=h+j}{=} \sum_{j=0}^{\infty}a^{h+j}a^{j}\sigma_U^2 = a^h\sum_{j=0}^{\infty}a^{2j}\sigma_U^2
+# = \frac{a^h}{1-a^2}\sigma_U^2 \in \mathbb{R}
+# $$
+#
+# Somit ist $E[Y_t]=0$ und $\gamma(t, h)$ hängt nicht von $t$ ab. Folglich ist $(Y_t)_{t \in \mathbb{Z}}$ stationär. &#11035;
+
+# %% [markdown] slideshow={"slide_type": "notes"}
+# - Noch offener Punkt, wenn X_h to X, wieso ist dann E[Y_t] = E[X]? Also warum passt das alles?
+# - [Fubini-Tonelli Theorems](https://en.wikipedia.org/wiki/Fubini's_theorem#Fubini-Tonelli)
+# https://math.stackexchange.com/questions/1166994/linearity-of-expectation-for-infinite-sums
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# ## Testen auf Stationarität -> Am ende machen, low Priority
 
 # %% [markdown] slideshow={"slide_type": "slide"}
 # Angenommen unsere Daten werden durch den vorher beschriebenen AR(1) Prozess generiert, wie könnte somit ein Test auf (schwache) Stationarität aussehen?
@@ -138,7 +303,7 @@ Für die statistische Inferenz mit Zeitreihen müssen Annahmen getroffen werden,
 # 3. Da in der Praxis in der Regel a >= 0, Vereinfachung zu H_0: a >= 1 vs. H_1: a < 1
 
 # %% [markdown] slideshow={"slide_type": "slide"}
-# ## The (augmented) Dickey-Fuller Test
+# ## Der (Augmented) Dickey-Fuller Test
 # 5. Der Dickey-Fuller Test macht das im Prinzip (verwendet aber die Differenzen)
 # 5. Erweiterung zum Augmented Dickey-Fuller Test
 #
@@ -163,7 +328,17 @@ Für die statistische Inferenz mit Zeitreihen müssen Annahmen getroffen werden,
 #
 
 # %% [markdown] slideshow={"slide_type": "slide"}
-# # Praktische Anwendung
+# # Praktische Anwendung: Modellierung und Prognose der Inflationsrate
+
+# %% [markdown] slideshow={"slide_type": "notes"}
+# - Keine Kreuzvalidierung nötig, da least-squares benutzt wird, man könnten den autoregressiven Parameter natürlich penalisieren und dann tunen
+# - Fitten bis 2018, dann Prognose
+# - fitten bis 2020 (vor Corona und Wirtschaftskrise), dann Prognose
+# - Diskussion: Ist da ein Strukturbruch passiert? Stationarität verletzt? Es ist möglich, wenn auch eher unwahrscheinlich, dass die Zeitreihen so ansteigt (mit Simulation rausfinden wie wahrscheinlich das ist, dass so ein Trend mindestens beobachtet wird)
+# - Rein datenbasiert das schwer zu beantworten. Eher Expertenwissen oder komplexere Modell wo externe Faktoren eine Rolle spielen (Konsumausgaben, Wirtschaftliche Entwicklung, was treibt die Inflation)
 
 # %% [markdown] slideshow={"slide_type": "slide"}
 # # Literatur
+# Hamilton
+#
+# Lütkepol MTSA
